bba567cf7d011e2e89e82ca46574bedfedad2bb6
Hammadi15 <hammadiaali3@gmail.com>
2025-01-21 15:22:11 +0100

    added comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Card from "@/components/Card";
 export default function Home() {
   return <>
   <div className="flex flex-wrap gap-x-12">
-  <Card name="test" userName = "Nermin"/>
+  <Card name="test" userName = "d"/>
   <Card name="test" userName = "Nermin"/>
 
   </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,29 +3,30 @@ import React from "react";
 import Image from "next/image";
 import pic1 from "../app/pic/pic1.png";
 type CardProps = {
-  name: string;
-  userName: string;
+  //created props
+  name: string; //name string
+  userName: string; //usewrname srting
 };
 
 const Card = ({ name, userName }: CardProps) => {
+  //created prop both name,userName
   return (
     <>
       <div className="card bg-base-100 w-96 shadow-xl">
+        {" "}
+        {/* created dasyUi Compoment */}
         <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-
+          <h2 className="card-title">{userName}</h2> {/*  props with userName*/}
           <div className="flex items-center">
-            <div className=" h-12 w-12">
-              <Image src={pic1} alt="Test" />
+            <div className=" h-10 w-10">
+              <Image src={pic1} alt="Test" /> {/* picture */}
             </div>
-            <button className="btn btn-xs">+</button>
+            <button className="btn btn-xs">+</button> {/* created button*/}
             <p className=" flex-grow-0 ml-0">{name}</p>{" "}
             <button className="btn btn-xs">-</button>
           </div>
-       
-
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">Buy Now-</button>
           </div>
         </div>
       </div>
