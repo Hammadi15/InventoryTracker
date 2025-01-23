988f9e095b977ce1d87673b40c079220bcc836c5
Hammadi15 <hammadiaali3@gmail.com>
2025-01-23 13:51:42 +0100

    added buttons

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import pic1 from "../app/pic/pic1.png";
 type CardProps = {
@@ -9,7 +9,18 @@ type CardProps = {
 };
 
 const Card = ({ name, userName }: CardProps) => {
+  const [count, setCount] = useState(0);
+
   //created prop both name,userName
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <>
       <div className="card bg-base-100 w-96 shadow-xl">
@@ -21,8 +32,11 @@ const Card = ({ name, userName }: CardProps) => {
             <div className=" h-10 w-10">
               <Image src={pic1} alt="Test" /> {/* picture */}
             </div>
-            <button className="btn btn-xs">+</button> {/* created button*/}
-            <p className=" flex-grow-0 ml-0">{name}</p>{" "}
+            <button onClick={handleIncrease} className="btn btn-xs">
+              +
+            </button>{" "}
+            {/* created button*/}
+            <label ></label>
             <button className="btn btn-xs">-</button>
           </div>
           <div className="card-actions justify-end">
